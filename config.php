bda16193359cfcee0dafdc69d12a57b55aac9ed8
himanshu gohel <himanshugohel07@gmail.com>
2025-07-09 00:41:11 +0530

    1

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
-$db_host = getenv("MYSQLHOST");
-$db_user = getenv("MYSQLUSER");
-$db_pass = getenv("MYSQLPASSWORD");
-$db_name = getenv("MYSQLDATABASE");
+$db_host = "mysql.railway.internal";       // from Railway MYSQLHOST
+$db_user = "root";       // from MYSQLUSER
+$db_pass = "OurQYtIpxPGhftGrKfCfxqQZEKTwKrsq";   // from MYSQLPASSWORD
+$db_name = "railway";    // from MYSQLDATABASE
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully!";
 ?>
